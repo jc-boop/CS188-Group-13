7aeedcaa0a3afc7020f4035e8c53d3c3835e9ebe
Jason Rauchwerk <jrauchwerk@gmail.com>
2022-01-30 22:08:06 -0800

    First implementation of semeval __getitem__ - copy of dummy implementation

--- data_processing/processors.py
+++ data_processing/processors.py
@@ -136,7 +136,24 @@ class SemEvalDataset(Dataset):
         # the outputs of tokenizer for certain types of
         # models (e.g. RoBERTa), please take special care
         # of it with an if-else statement.
-        raise NotImplementedError("Please finish the TODO!")
+        example = self.examples[idx]
+        guid = example.guid
+        text = example.text
+
+        batch_encoding = self.tokenizer(
+                text,
+                add_special_tokens=True,
+                max_length=self.max_seq_length,
+                padding="max_length",
+                truncation=True,
+        )
+
+        input_ids = torch.Tensor(batch_encoding['input_ids']).long()
+        attention_mask = torch.Tensor(batch_encoding['attention_mask']).long()
+        if 'token_type_ids' not in batch_encoding:
+            token_type_ids = torch.zeros_like(input_ids)
+        else:
+            token_type_ids = torch.Tensor(batch_encoding['token_type_ids']).long()
         # End of TODO.
         ##################################################
 
